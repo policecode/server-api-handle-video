5b99511047a17b473829c53d365739a875f02c12
Nguyễn Hoàng Đạt <dat01655993202@gmail.com>
2025-08-10 15:42:34 +0700

    update

--- routes/toolvideo.js
+++ routes/toolvideo.js
@@ -6,6 +6,11 @@ webApp.get('/api/tool-video', async (req, res) => {
   return res.json('API này có hoạt động haiz ok men')
 });
 
+webApp.post('/api/tool-video/crawl-download-test', async (req, res) => {
+  const toolVideoController = new ToolVideoController();
+  return toolVideoController.testToolVideo(req, res);
+});
+
 webApp.post('/api/tool-video/crawl-download-youtube', async (req, res) => {
   const toolVideoController = new ToolVideoController();
   return toolVideoController.crawlYoutube(req, res);
